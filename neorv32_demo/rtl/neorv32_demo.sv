1c17e34dd2ff524e4fec548e5aa9d74c603d549f
Gus <gtaharaedmonds@gmail.com>
2025-03-07 23:39:27 +0000

    wip

---
+++ neorv32_demo/rtl/neorv32_demo.sv
@@ -0,0 +1,18 @@
+module neorv32_demo (
+    input logic sys_clk,
+    input logic rst_n,
+    input logic [7:0] gpio_i,
+    output logic [7:0] gpio_o
+);
+  neorv32_system neorv32_system_unit (
+      .*,
+      .time_irq(0),
+      .sw_irq  (0),
+      .ext_irq (0),
+      .jtag_tdi(),
+      .jtag_tdo(),
+      .jtag_tck(),
+      .jtag_tms()
+  );
+
+endmodule
